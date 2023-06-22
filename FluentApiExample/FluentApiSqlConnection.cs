5a04d55cc25f289752bf3db0e38673bc7acb2b83
Serghei Dvorjanskii <it.serghei@gmail.com>
2023-06-22 18:28:23 +0300

    Fluent API example

---
+++ FluentApiExample/FluentApiSqlConnection.cs
@@ -0,0 +1,86 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace FluentApiExample;
+
+public class FluentApiSqlConnection : IServerSelectionStage, IDatabaseSelectionStage, IUserSelectionStage,
+    IPasswordSelectionStage, IConnectionInitializerStage
+{
+    private string _server;
+    private string _database;
+    private string _user;
+    private string _password;
+
+    private FluentApiSqlConnection()
+    {
+    }
+
+    public static IServerSelectionStage CreateConnection(Action<ConnectionConfiguration> config)
+    {
+        var configuration = new ConnectionConfiguration();
+        config?.Invoke(configuration); 
+        return new FluentApiSqlConnection();
+    }
+
+    public IDatabaseSelectionStage ForServer(string server)
+    {
+        _server = server;
+        return this;
+    }
+
+    public IUserSelectionStage ForDatabase(string database)
+    {
+        _database = database;
+        return this;
+    }
+
+    public IPasswordSelectionStage ForUser(string user)
+    {
+        _user = user;
+        return this;
+    }
+
+    public IConnectionInitializerStage WithPassword(string password)
+    {
+        _password = password;
+        return this;
+    }
+
+    public IDbConnection Connect()
+    {
+        var connection =
+            new SqlConnection($"Server={_server};Database={_database};User Id={_user};Password={_password}");
+        connection.Open();
+        return connection;
+    }
+}
+
+public class ConnectionConfiguration
+{
+    public string ConnectionName { get; set; }
+}
+
+public interface IServerSelectionStage
+{
+    public IDatabaseSelectionStage ForServer(string server);
+}
+
+public interface IDatabaseSelectionStage
+{
+    public IUserSelectionStage ForDatabase(string database);
+}
+
+public interface IUserSelectionStage
+{
+    public IPasswordSelectionStage ForUser(string user);
+}
+
+public interface IPasswordSelectionStage
+{
+    public IConnectionInitializerStage WithPassword(string password);
+}
+
+public interface IConnectionInitializerStage
+{
+    public IDbConnection Connect();
+}
---
+++ FluentApiExample/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using FluentApiExample;
+using Microsoft.IdentityModel.Protocols;
+
+var connection = FluentApiSqlConnection
+    .CreateConnection(conf =>
+    {
+        conf.ConnectionName = "My connection"; // just for testing
+    })
+    .ForServer("server")
+    .ForDatabase("database")
+    .ForUser("user")
+    .WithPassword("password")
+    .Connect();
+
+
+Console.WriteLine("Hello, World!");
